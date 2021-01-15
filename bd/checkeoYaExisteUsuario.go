ab61ea11132005c1d8413fa15714059965c1d043
JoaoPaulo87 <correaparisejoaopaulo@gmail.com>
2021-01-15 10:15:18 -0300

    Commit del registro de usuarios

--- bd/checkeoYaExisteUsuario.go
+++ bd/checkeoYaExisteUsuario.go
@@ -11,13 +11,18 @@ import (
 /*ChequeoYaExisteUsuario recibe un mail de parámetro y chequea si ya
 está en la BD*/
 func ChequeoYaExisteUsuario(email string) (models.Usuario, bool, string) {
+	//ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	ctx, _ := context.WithTimeout(context.Background(), 15*time.Second)
 	db := MongoCN.Database("microblogging")
 	col := db.Collection("usuarios")
 
+	// M es una función que formatea o mapea a bson lo que recibe como json
 	condicion := bson.M{"email": email} //Le pasamos un json
+
+	// en la variable resultado voy a modelar un usuario
 	var resultado models.Usuario
 
+	//FindOne me devuelve un sólo registro que cumple con la condición
 	err := col.FindOne(ctx, condicion).Decode(&resultado)
 	ID := resultado.ID.Hex()
 
